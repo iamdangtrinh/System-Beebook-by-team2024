921cdce88ab7dd45850f1323fc9e97af94e9525b
nnqn15 <nnqn15@gmail.com>
2024-08-30 14:53:57 +0700

    Update CouponSeeder.php

--- database/seeders/CouponSeeder.php
+++ database/seeders/CouponSeeder.php
@@ -15,8 +15,8 @@ class CouponSeeder extends Seeder
     public function run(): void
     {
         // Define coupon types
-        $couponTypes = [1, 2]; // 1 = Percentage, 2 = Fixed Amount
-        $statuses = ['active', 'inactive'];
+        $couponTypes = ['percent', 'amount']; // 'percent' = Percentage, 'amount' = Fixed Amount
+        $statuses = ['active', 'inactive', 'expired'];
 
         // Define the number of coupons to create
         $numberOfCoupons = 100;
@@ -27,23 +27,29 @@ class CouponSeeder extends Seeder
             $typeCoupon = Arr::random($couponTypes);
             $quantity = rand(1, 100);
             $status = Arr::random($statuses);
+            $couponMinSpend = rand(10000, 50000); // Random minimum spend between 10k and 50k
+            $couponMaxSpend = $couponMinSpend + rand(50000, 200000); // Random max spend higher than min spend
 
             // Determine the discount value based on the coupon type
-            if ($typeCoupon === 1) {
+            if ($typeCoupon === 'percent') {
                 // Percentage discount
                 $discount = rand(5, 30); // Random percentage between 5% and 30%
-                $description= "Giảm " . $discount . "% áp dụng cho toàn bộ sản phẩm";
+                $description = "Giảm " . $discount . "% áp dụng cho toàn bộ sản phẩm";
             } else {
                 // Fixed amount discount
                 $discount = rand(5000, 50000); // Random fixed amount between 5k and 50k
-                $description= "Giảm " .number_format($discount, 0, ',', '.'). "đ  áp dụng cho toàn bộ sản phẩm";
+                $description = "Giảm " . number_format($discount, 0, ',', '.') . "đ áp dụng cho toàn bộ sản phẩm";
             }
 
             DB::table('coupons')->insert([
                 'code_coupon' => $codeCoupon,
                 'description' => $description,
+                'start_date' => now(),
                 'expires_at' => $expiresAt,
+                'coupon_min_spend' => $couponMinSpend,
+                'coupon_max_spend' => $couponMaxSpend,
                 'discount' => $discount,
+                'coupon_uses_per_customer' => rand(1, 5), // Random number of uses per customer between 1 and 5
                 'type_coupon' => $typeCoupon,
                 'quantity' => $quantity,
                 'status' => $status,
