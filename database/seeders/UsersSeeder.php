16b80e6d6869c4fcb2f4a62b1e7302d7cf527896
Anhtien2018 <134468299+Anhtien2018@users.noreply.github.com>
2024-09-06 22:17:16 +0700

    update seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
@@ -12,6 +16,26 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create('vi_VN');
+
+        foreach (range(1, 10) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'phone' => substr($faker->phoneNumber, 0, 10),
+                'roles' => $faker->randomElement(['admin', 'customer']),
+                'status' => $faker->randomElement(['active', 'inactive']),
+                'image' => 'path/to/image' . $index . '.jpg',
+                'facebook_id' => $faker->optional()->randomNumber(),
+                'google_id' => $faker->optional()->randomNumber(),
+                'address' => $faker->address,
+                'birthday' => $faker->date('Y-m-d', '2000-01-01'),
+            ]);
     }
+}
 }
