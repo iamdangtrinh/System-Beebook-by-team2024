342903e3507cf5a992b457eb6b8e2aab32c938b4
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-10-13 13:04:01 +0700

    Update CartService.php

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -49,7 +49,7 @@ class CartService implements CartServiceInterface
             // Khởi tạo mảng chứa các ID sản phẩm từ giỏ hàng
             $proIdArr = array_column($carts, 'product_id');
             // Truy vấn sản phẩm từ cơ sở dữ liệu dựa trên các product_id
-            $products = Product::whereIn('id', $proIdArr)->get();
+            $products = Product::whereIn('id', $proIdArr)->get() ?? [];
             // Tạo một mảng để lưu kết hợp dữ liệu từ giỏ hàng và sản phẩm
             $cart = [];
             foreach ($products as $product) {
