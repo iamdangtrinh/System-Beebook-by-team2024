e123f76310c3c69393f260fda0d0d7abaa848ce2
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-10-15 09:45:56 +0700

    update api ghn

--- app/Http/Controllers/client/getLocationGHNContronller.php
+++ app/Http/Controllers/client/getLocationGHNContronller.php
@@ -7,5 +7,41 @@ use Illuminate\Http\Request;
 
 class getLocationGHNContronller extends Controller
 {
-    
+      private string $URL_API;
+      private string $GHN_TOKEN;
+
+      public function __construct()
+      {
+            $this->URL_API = env('URL_GHN_API');
+            $this->GHN_TOKEN = env('GHN_TOKEN');
+      }
+
+      public function getProvincer() {
+            return $this->sendRequestGHN('province');
+      }
+      
+      public function getDistrict() {
+            return $this->sendRequestGHN('district', 269);
+      }
+      public function getWard() {
+            return $this->sendRequestGHN('province');
+      }
+
+      protected function sendRequestGHN($endpoint, $id = "")
+      {
+            $ch = curl_init($this->URL_API . $endpoint. $id);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                  'Content-Type: application/json',
+                  'Token: ' . $this->GHN_TOKEN
+            ));
+            $response = curl_exec($ch);
+            curl_close($ch);
+            if ($response === false) {
+                  echo 'Lỗi khi kết nối tới API';
+                  return null;
+            }
+            $result = json_decode($response, true);
+            return $result;
+      }
 }
