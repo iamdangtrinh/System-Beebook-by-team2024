6f88783d3cfab5c2ff9540e3fbc437dd9654e669
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-10-29 18:43:29 +0700

    admin to backend

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function index()
+    {
+        return view('admin.index');
+    }
+    
     public function show404()
     {
         return view('admin.404');
--- routes/route/phatroute.php
+++ routes/route/phatroute.php
@@ -19,12 +19,12 @@ Route::get('/posts/{slug}', [BlogController::class, 'show'])->name('posts.show')
 //admin
 
 Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index']);
     Route::get('/404', [AdminController::class, 'show404']);
     Route::get('/500', [AdminController::class, 'show500']);
     Route::get('/blogs', [AdminController::class, 'blogs']);
     Route::get('/article', [AdminController::class, 'article']);
     Route::get('/dashboard_3', [AdminController::class, 'dashboard_3']);
-    
 });
 
 // Route::get('/activity_stream', [AdminController::class, 'activity_stream']);
