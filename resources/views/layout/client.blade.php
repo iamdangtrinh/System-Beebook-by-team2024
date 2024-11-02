ad58508bb507c4dfe85098eebdf9f740de7fcc3f
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-11-02 21:26:00 +0700

    hot fix v2

--- resources/views/layout/client.blade.php
+++ resources/views/layout/client.blade.php
@@ -1,9 +1,5 @@
- <!-- Include phần header -->
     @include('Client.components.header')
     @yield('body')
-    <!-- Include phần footer -->
     @include('Client.components.footer')
-
-    <!-- Include script nếu cần -->
 </body>
 </html>
