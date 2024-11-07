8ea1377dd3b0da2ef786fdec221dc4bacf6ab063
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-11-08 00:19:51 +0700

    ...

--- public/client/js/qr-checkout.js
+++ public/client/js/qr-checkout.js
@@ -79,7 +79,7 @@
     };
 
     $(document).ready(function () {
-        // DT.checkStatus();
-          DT.timerCheckout();
+        DT.checkStatus();
+        //   DT.timerCheckout();
     });
 })(jQuery);
