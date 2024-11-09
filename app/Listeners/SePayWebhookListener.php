283f1d14d05878904f2c3c5d50176afb21d75d29
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-11-09 17:11:37 +0700

    test send email v1

--- app/Listeners/SePayWebhookListener.php
+++ app/Listeners/SePayWebhookListener.php
@@ -6,8 +6,8 @@ use App\Models\BillModel;
 use App\Models\User;
 use SePay\SePay\Events\SePayWebhookEvent;
 use SePay\SePay\Notifications\SePayTopUpSuccessNotification;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class SePayWebhookListener
 {
@@ -27,25 +27,26 @@ class SePayWebhookListener
         // Xử lý tiền vào tài khoản
         if ($event->sePayWebhookData->transferType === 'in') {
 
-            $emailBought = BillModel::where('id', $event->info)->pluck('email')->first();
-            Mail::raw('Order: ' . $emailBought, function ($message) {
-                $message->to('dtrinhit84@gmail.com')
-                    ->subject('Hello Email');
-            });
+            // $emailBought = BillModel::where('id', $event->info)->pluck('email')->first();
+            // Mail::raw('Order: ' . $emailBought, function ($message) {
+            //     $message->to('dtrinhit84@gmail.com')
+            //         ->subject('Hello Email');
+            // });
 
-            try {
-                if ($emailBought) {
-                    Mail::to('dtrinhit84@gmail.com')->send(new \App\Mail\sendEmailOrder($event->info));
-                    Mail::to($emailBought)->send(new \App\Mail\sendEmailOrder($event->info));
-                }
-            } catch (\Exception $e) {
-                Log::error('Email sending failed: ' . $e->getMessage());
-            }
-
-            // $user = User::query()->where('id', $event->info)->first();
-            // if ($user instanceof User) {
-            //     $user->notify(new SePayTopUpSuccessNotification($event->sePayWebhookData));
+            // try {
+            //     if ($emailBought) {
+            //         Mail::to('dtrinhit84@gmail.com')->send(new \App\Mail\sendEmailOrder($event->info));
+            //         Mail::to($emailBought)->send(new \App\Mail\sendEmailOrder($event->info));
+            //     }
+            // } catch (\Exception $e) {
+            //     Log::error('Email sending failed: ' . $e->getMessage());
             // }
+
+            $emailBought = BillModel::where('id', $event->info)->pluck('email')->first();
+            $user = User::query()->where('email', $emailBought)->first();
+            if ($user instanceof User) {
+                $user->notify(new SePayTopUpSuccessNotification($event->sePayWebhookData));
+            }
             // redirect()->route('thankyou.index', ['id' => ($event->info)]);
         } else {
             // Xử lý tiền ra tài khoản
