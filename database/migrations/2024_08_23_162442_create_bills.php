5234351e6e67bb8483c83b9d4474b1f8eeeb6cba
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-11-27 19:34:15 +0700

    update db

--- database/migrations/2024_08_23_162442_create_bills.php
+++ database/migrations/2024_08_23_162442_create_bills.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('bills', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_coupon');
             $table->enum('status', ['new', 'shipping', 'success', 'cancel', 'refund'])->default('new');
             $table->string('reason_cancel')->nullable();
             $table->decimal('total_amount', 15, 0);
@@ -30,6 +31,7 @@ return new class extends Migration
             $table->text('note_admin')->nullable();
             $table->timestamps();
 
+            $table->foreign('id_coupon')->references('id')->on('coupons')->onDelete('cascade');
             $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
         });
     }
