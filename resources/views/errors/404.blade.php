42959870f963cd1608e8aaa12d8aab2a8b4ee7db
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-11-30 13:16:34 +0700

    update page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,56 +1,63 @@
 <!DOCTYPE html>
 <html lang="vi">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 Không tìm thấy nội dung</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        body {
-            text-align: center;
-            padding: 40px 0;
-            background-color: #ffffff;
-        }
-        h1 {
+        .title {
             font-size: 10rem;
+            font-weight: 800;
             background: linear-gradient(to right, #ff6565, #ffa849, #ffeb3b, #7fc9ff);
             color: transparent;
             -webkit-background-clip: text;
             background-clip: text;
             margin-bottom: 0;
         }
+
         .lead {
-            font-size: 1.5rem;
-            font-weight: bold;
-            margin-bottom: 20px;
-            color: #000000;
+            font-size: 1.5rem !important;
+            font-weight: bold !important;
+            margin-bottom: 20px !important;
+            color: #000000 !important;
         }
+
         p {
             color: #666666;
         }
+
         .emoji {
             font-size: 2rem;
             margin-left: 10px;
             vertical-align: middle;
         }
+
         .btn-primary {
             padding: 10px 20px;
             font-size: 1rem;
             background-color: #007bff;
             border: none;
         }
+
         .btn-primary:hover {
             background-color: #0056b3;
         }
+
+        .__custom_container {
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+            height: 46vh;
+            justify-content: center;
+        }
     </style>
-</head>
-<body>
-    <div class="container">
-        <h1>404</h1>
+    @include('Client.components.header')
+    <div class="container __custom_container">
+        <h1 class="title">404</h1>
         <p class="lead">Không tìm thấy nội dung <span class="emoji">😓</span></p>
         <p>URL của nội dung này đã bị thay đổi hoặc không còn tồn tại.</p>
         <p>Nếu bạn đang lưu URL này, hãy thử truy cập lại từ trang chủ thay vì dùng URL đã lưu.</p>
         <a href="/" class="btn btn-primary">Về trang chủ</a>
     </div>
-</body>
-</html>
+    @include('Client.components.footer')
