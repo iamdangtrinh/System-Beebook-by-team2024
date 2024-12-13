8fd6b2812b7b5b0f9c5ee7d11b078b0b6009090a
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2024-12-13 21:50:17 +0700

    Update blog-admin.blade.php

--- resources/views/livewire/blog-admin.blade.php
+++ resources/views/livewire/blog-admin.blade.php
@@ -1,11 +1,10 @@
 <div>
     <div class="row wrapper border-bottom white-bg page-heading">
-        <div class="col-lg-9">
+        <div class="col-lg-8">
 
         </div>
-        <div class="col-lg-3 d-flex" style="gap: 20px">
+        <div class="col-lg-4 d-flex" style="gap: 20px">
             <a href="{{route('adminblog.trash')}}" class="btn btn-primary">Bài viết đã xóa</a>
-            {{-- <a wire:click="closeModal" class="btn btn-outline btn-primary btn-rounded">Thêm bài viết</a> --}}
             <a href="{{route('adminblog.show')}}" class="btn btn-primary">Thêm bài viết</a>
         </div>
     </div>
